160a8bc3ed4166fa0a3eaa2c651b9d6d6f0f8fb6
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-02-01 18:14:03 +0430

    adding pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 function App() {
   return <div className="bg-red-200">App</div>;
 }
---
+++ src/pages/Mangas.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mangas() {
+  return <div>Mangas</div>;
+}
+
+export default Mangas;
