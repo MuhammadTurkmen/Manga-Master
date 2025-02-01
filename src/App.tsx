12704d6d0c9bd7fe1321b2657a098262438088db
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-02-01 19:17:03 +0430

    adding page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+import About from "./pages/About";
 
-const router = createBrowserRouter([{}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
